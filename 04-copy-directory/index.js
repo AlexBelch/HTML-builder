ab4c8c5c906e08b14e8e3816c6d240a7bf13b506
Alexander BELCHITSKIY <alexander.belchitskiy@1000geeks.com>
2022-05-23 22:33:54 +0300

    feat: add solution subtask "04 copy directory"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
 
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const dir = path.join(__dirname, 'files');
+const dircopy = path.join(__dirname, 'files-copy');
+
+fs.rm(dircopy, {
+  recursive: true,
+  force: true
+}).finally(() => {
+  fs.mkdir(dircopy, {
+    recursive: true
+  });
+
+  fs.readdir(dir, {
+    withFileTypes: true
+  }).then(data => {
+    data.forEach(item => {
+      if (item.isFile()) {
+        const pathItem = path.join(dir, item.name);
+        const pathItemCopy = path.join(dircopy, item.name);
+        fs.copyFile(pathItem, pathItemCopy);
+      }
+    });
+  });
+});
